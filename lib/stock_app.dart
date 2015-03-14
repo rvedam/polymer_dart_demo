593e391402ec2b4472ee807a5d8a131dfcb03fea
Ramnarayan Vedam <rvedam@gmail.com>
2015-03-14 15:18:06 -0400

    added new StockAppElement

---
+++ lib/stock_app.dart
@@ -0,0 +1,9 @@
+import 'package:polymer/polymer.dart';
+import 'dart:html' show Event, Node;
+
+@CustomTag('stock-app')
+class StockAppElement extends PolymerElement {
+  StockAppElement.created() : super.created() {
+    
+  }
+}
